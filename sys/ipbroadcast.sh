eb8090b95c2e95065c26022486b541dcabd92de0
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-06-11 21:02:32 +0100

    🚚 Refactor ipbroadcast.sh to simplify IP address serving by removing the Python server implementation

--- sys/ipbroadcast.sh
+++ sys/ipbroadcast.sh
@@ -3,25 +3,11 @@
 # Get the primary IP address (first non-loopback)
 IP=$(hostname -I | awk '{print $1}')
 
-# Create a temporary Python script to serve the IP address
-PYFILE="/tmp/ipbroadcast_server.py"
-cat > "$PYFILE" <<EOF
-import http.server
-import socketserver
+PORT=246
 
-PORT = 246
-IP = "$IP"
-
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(f"<h1>Device IP: {IP}</h1>".encode())
-
-with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
-    httpd.serve_forever()
-EOF
-
-# Run the Python server in the background
-python3 "$PYFILE" &
+while true; do
+    # Wait for a connection and serve the IP address as a simple HTML page
+    { 
+        echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Device IP: $IP</h1>";
+    } | nc -l -p $PORT -q 1
+done
