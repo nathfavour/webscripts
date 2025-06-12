d76a8b0c47bdf872d8b2b2a214a07a6428868b65
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-06-12 07:27:35 +0100

    🐛 Fix ipbroadcast.sh to remove deprecated '-p' option from nc command

--- sys/ipbroadcast.sh
+++ sys/ipbroadcast.sh
@@ -9,5 +9,5 @@ while true; do
     # Wait for a connection and serve the IP address as a simple HTML page
     { 
         echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Device IP: $IP</h1>";
-    } | nc -l -p $PORT -q 1
+    } | nc -l $PORT -q 1
 done
